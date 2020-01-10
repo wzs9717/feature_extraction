23ec06c2920842a533bf97500e6d0687fbb2a52e
root <wang1411@e.ntu.edu,sg>
2020-01-10 17:34:14 +0800

    confusion max added

--- test.py
+++ test.py
@@ -1,51 +1,209 @@
-import os
-# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+#!/usr/bin/env python
+# encoding: utf-8
 
-import keras
-import sys
-sys.setrecursionlimit(10000)
 
-#import densenet
-import numpy as np
-import sklearn.metrics as metrics
-from keras.datasets import cifar100
-from keras.utils import np_utils
-from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import Adam
-from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
-from keras import backend as K
-from keras.preprocessing.image import img_to_array, array_to_img
-from keras.models import Sequential,load_model
-from PIL import Image
+# import tensorflow as tf
+# import datetime
+# import numpy as np
+# mnist = tf.keras.datasets.mnist
+
+# log_dir="logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+# (x_train, y_train),(x_test, y_test) = mnist.load_data()
+# x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# def create_model():
+#     return tf.keras.models.Sequential([
+#     tf.keras.layers.Flatten(input_shape=(28, 28)),
+#     # tf.keras.layers.Dense(512, activation='relu'),
+#     # tf.keras.layers.Dropout(0.2),
+#     tf.keras.layers.Dense(10, activation='softmax')
+#   ])
+
+# model = create_model()
+# model.compile(optimizer='adam',
+#               loss='sparse_categorical_crossentropy',
+#               metrics=['accuracy'])
+# img = np.reshape(x_train[0], (-1, 28, 28, 1))
+# # file_writer = tf.summary.create_file_writer(log_dir)
+# # with file_writer.as_default():
+# #     tf.summary.image("Training data", img, step=0)
+# tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=0)
+
+# model.fit(x=x_train, 
+#           y=y_train, 
+#           epochs=5, 
+#           validation_data=(x_test, y_test), 
+#           callbacks=[tensorboard_callback])
+
+
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from datetime import datetime
+import io
+import itertools
+from packaging import version
+from six.moves import range
+
 import tensorflow as tf
-import gc
-import pandas as pd
-from keras.layers import Input, Conv2D, GlobalAveragePooling2D, Dropout
-from keras.layers import Activation, BatchNormalization, Add, Reshape, DepthwiseConv2D
+from tensorflow import keras
 
-from keras.utils import multi_gpu_model
-from keras import backend as K
 import matplotlib.pyplot as plt
-import cv2
-from scipy import io
-import struct
+import numpy as np
+import sklearn.metrics
+mnist = tf.keras.datasets.mnist
+print("TensorFlow version: ", tf.__version__)
+assert version.parse(tf.__version__).release[0] >= 2, \
+    "This notebook requires TensorFlow 2.0 or above."
 
-def pic_transfer(X):
-    # pic_tem2=X.resize((224, 224),Image.BILINEAR)
-    X_resize=img_to_array(X)
-    return X_resize
+    # Download the data. The data is already divided into train and test.
+# The labels are integers representing classes.
+# fashion_mnist = keras.datasets.fashion_mnist
+# (train_images, train_labels), (test_images, test_labels) = \
+#     fashion_mnist.load_data()
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+train_images, test_images = train_images / 255.0, test_images / 255.0
+# Names of the integer classes, i.e., 0 -> T-short/top, 1 -> Trouser, etc.
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+    'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-def main():
-    model=load_model('./weights/MobilenetV2-CIFAR100.h5')
+print("Shape: ", train_images[0].shape)
+print("Label: ", train_labels[0], "->", class_names[train_labels[0]])
 
-    layer_1 = K.function([model.layers[0].input], [model.get_layer('global_average_pooling2d_1').output])
-    images=cv2.imread('./data/train/'+str(0)+'.jpg')
-    data = np.expand_dims(images, axis=0)
-    # data = pic_transfer(images)
-    print(np.max(data[:]))
-    # print(images.shape)
-    f1 = layer_1([data/ 255.0])[0]
-    print(f1.shape)
-if __name__ == '__main__':
-    main()
+
+# # Reshape the image for the Summary API.
+# img = np.reshape(train_images[0], (-1, 28, 28, 1))
+
+# # Sets up a timestamped log directory.
+# logdir = "logs/train_data/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+# # Creates a file writer for the log directory.
+# file_writer = tf.summary.create_file_writer(logdir)
+
+# # Using the file writer, log the reshaped image.
+# with file_writer.as_default():
+#   # Don't forget to reshape.
+#   images = np.reshape(train_images[0:25], (-1, 28, 28, 1))
+#   tf.summary.image("25 training data examples", images, max_outputs=25, step=0)
+
+
+# logdir = "logs/plots/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+# file_writer = tf.summary.create_file_writer(logdir)
+
+def plot_to_image(figure):
+  """Converts the matplotlib plot specified by 'figure' to a PNG image and
+  returns it. The supplied figure is closed and inaccessible after this call."""
+  # Save the plot to a PNG in memory.
+  buf = io.BytesIO()
+  plt.savefig(buf, format='png')
+  # Closing the figure prevents it from being displayed directly inside
+  # the notebook.
+  plt.close(figure)
+  buf.seek(0)
+  # Convert PNG buffer to TF image
+  image = tf.image.decode_png(buf.getvalue(), channels=4)
+  # Add the batch dimension
+  image = tf.expand_dims(image, 0)
+  return image
+
+# def image_grid():
+#   """Return a 5x5 grid of the MNIST images as a matplotlib figure."""
+#   # Create a figure to contain the plot.
+#   figure = plt.figure(figsize=(10,10))
+#   for i in range(25):
+#     # Start next subplot.
+#     plt.subplot(5, 5, i + 1, title=class_names[train_labels[i]])
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+  
+#   return figure
+
+# # Prepare the plot
+# figure = image_grid()
+# # Convert to image and log
+# with file_writer.as_default():
+#   tf.summary.image("Training data", plot_to_image(figure), step=0)
+
+#-----------------------------------------------
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    # keras.layers.Dense(32, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(
+    optimizer='adam', 
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+
+def plot_confusion_matrix(cm, class_names):
+  """
+  Returns a matplotlib figure containing the plotted confusion matrix.
+
+  Args:
+    cm (array, shape = [n, n]): a confusion matrix of integer classes
+    class_names (array, shape = [n]): String names of the integer classes
+  """
+  figure = plt.figure(figsize=(8, 8))
+  plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+  plt.title("Confusion matrix")
+  plt.colorbar()
+  tick_marks = np.arange(len(class_names))
+  plt.xticks(tick_marks, class_names, rotation=45)
+  plt.yticks(tick_marks, class_names)
+
+  # Normalize the confusion matrix.
+  cm = np.around(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=2)
+  # print(cm.type)
+  # Use white text if squares are dark; otherwise black.
+  threshold = cm.max() / 2.
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    color = "white" if cm[i, j] > threshold else "black"
+    plt.text(j, i, cm[i, j], horizontalalignment="center", color=color)
+
+  plt.tight_layout()
+  plt.ylabel('True label')
+  plt.xlabel('Predicted label')
+  return figure
+
+
+logdir = "logs/image/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+# Define the basic TensorBoard callback.
+tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
+file_writer_cm = tf.summary.create_file_writer(logdir + '/cm')
+
+
+def log_confusion_matrix(epoch, logs):
+  # Use the model to predict the values from the validation dataset.
+  test_pred_raw = model.predict(test_images)
+  test_pred = np.argmax(test_pred_raw, axis=1)
+
+  # Calculate the confusion matrix.
+  cm = sklearn.metrics.confusion_matrix(test_labels, test_pred)
+  # Log the confusion matrix as an image summary.
+  figure = plot_confusion_matrix(cm, class_names=class_names)
+  cm_image = plot_to_image(figure)
+
+  # Log the confusion matrix as an image summary.
+  with file_writer_cm.as_default():
+    tf.summary.image("Confusion Matrix", cm_image, step=epoch)
+
+# Define the per-epoch callback.
+cm_callback = keras.callbacks.LambdaCallback(on_epoch_end=log_confusion_matrix)
+
+
+# Train the classifier.
+model.fit(
+    train_images,
+    train_labels,
+    epochs=1,
+    # verbose=0, # Suppress chatty output
+    callbacks=[tensorboard_callback, cm_callback],
+    validation_data=(test_images, test_labels),
+)
