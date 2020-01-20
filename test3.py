f54baba52490621cdf21581eb0bee05942f3f923
root <wang1411@e.ntu.edu,sg>
2020-01-20 16:00:48 +0800

    cifar10

--- test3.py
+++ test3.py
@@ -11,20 +11,20 @@ import matplotlib.pyplot as plt
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 EAGER = True
  
-fashion_mnist = keras.datasets.fashion_mnist
-class_names = ['T-shirt/top','Trouser','Pullover','Dress','Coat','Sandal','Shirt','Sneaker','Bag','Ankle boot']
- 
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+cifar10 = keras.datasets.cifar10
+class_names = ['airplane', 'automoile', 'bird', 'cat', 'deer', 
+        'dog', 'frog', 'horse', 'ship', 'truck'] 
+(train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
  
  
 print(train_images.shape,train_labels.shape)
  
 
-train_images = train_images.reshape([-1,28,28,1]) / 255.0
-test_images = test_images.reshape([-1,28,28,1]) / 255.0
+train_images = train_images.reshape([-1,32,32,3]) / 255.0
+test_images = test_images.reshape([-1,32,32,3]) / 255.0
 classes=10
 chanDim=-1#TF BK
-inputShape=(28, 28, 1)
+inputShape=(32, 32, 3)
 model = tf.keras.Sequential()
 model.add(K.Conv2D(32, (3, 3), padding="same",
 			input_shape=inputShape))
@@ -54,22 +54,23 @@ model.add(K.BatchNormalization())
 model.add(K.Dropout(0.5))
 
 # softmax classifier
-model.add(K.Dense(classes))
-model.add(K.Activation("softmax"))
+model.add(K.Dense(classes,name="new_Dense"))
+model.add(K.Activation("softmax",name="new_softmax"))
  
-print(model.summary())
-model.load_weights("./weights/fashion_minist3.h5")
+# print(model.summary())
+model.load_weights("./weights/fashion_minist3.h5",by_name=False)
 # lr = 0.001
 epochs = 10
 checkpoint = keras.callbacks.ModelCheckpoint("./weights/fashion_minist3.h5",
             verbose=0,
-            save_best_only=False,
+            save_best_only=True,
             monitor="val_accuracy",
-            mode='max'
+            mode='max',
+            save_weights_only=False
         )
-opt = tf.keras.optimizers.Adam(lr=1e-5, decay=0)
-for i in range(1, 15):
-    model.layers[i].trainable = False
+opt = tf.keras.optimizers.Adam(lr=5e-4, decay=5e-7)
+# for i in range(1, 15):
+#     model.layers[i].trainable = False
 # 编译模型
 model.compile(optimizer=opt,
               loss='sparse_categorical_crossentropy',
@@ -78,9 +79,10 @@ model.compile(optimizer=opt,
 # 拟合数据
 model.fit(train_images, train_labels,
     epochs=epochs,
-    validation_data=[test_images[:5000],test_labels[:5000]],
-    batch_size=8,
-    callbacks=[checkpoint]
+    validation_data=[test_images,test_labels],
+    batch_size=256,
+    callbacks=[checkpoint],
+    verbose=1
     )
 
 # 模型评测
@@ -88,18 +90,17 @@ test_loss, test_acc = model.evaluate(test_images, test_labels,verbose=0)
  
 print('the model\'s test_loss is {} and test_acc is {}'.format(test_loss, test_acc))
  
-#部分预测结果展示
-show_images = test_images[:10]
-print(show_images.shape)
-predictions = model.predict(show_images)
-predict_labels = np.argmax(predictions, 1)
+# #部分预测结果展示
+# show_images = test_images[:10]
+# print(show_images.shape)
+# predictions = model.predict(show_images)
+# predict_labels = np.argmax(predictions, 1)
  
-plt.figure(figsize=(10,5)) #显示前10张图像，并在图像上显示类别
-for i in range(10):
-    plt.subplot(2,5,i+1)
-    plt.grid(False)
-    plt.imshow(show_images[i,:,:,0],cmap=plt.cm.binary)
-    plt.title(class_names[predict_labels[i]])
+# plt.figure(figsize=(10,5)) #显示前10张图像，并在图像上显示类别
+# for i in range(10):
+#     plt.subplot(2,5,i+1)
+#     plt.grid(False)
+#     plt.imshow(show_images[i,:,:,0],cmap=plt.cm.binary)
+#     plt.title(class_names[predict_labels[i]])
  
-plt.show()
-
+# plt.show()
